e2ef61e81f6d531f2a6998dacace8370fdff2708
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-03-28 02:41:21 +0700

    Enemy Spawner

---
+++ Assets/Script/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float spawnDelay;
+
+    public int enemyCount;
+    public int enemySpawned;
+
+    public Transform[] spawnPos;
+    public GameObject enemy;
+
+    IEnumerator RandomGenerate(float wait)
+    {
+        for (int i = 0; i < enemyCount; i++)
+        {
+            int random = Random.Range(0, spawnPos.Length);
+            if (spawnPos[random].childCount <1 && enemySpawned <= enemyCount)
+            {
+                enemy = Instantiate(enemy, spawnPos[random].position, Quaternion.identity);
+                enemy.transform.SetParent(spawnPos[random]);
+                enemySpawned++;
+                yield return new WaitForSeconds(wait);
+            }
+            else
+            {
+                i--;
+            }
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(RandomGenerate(spawnDelay));
+    }
+}
