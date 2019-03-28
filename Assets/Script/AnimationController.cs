e9dc21829586659512dd5702c2f06e515bba0e28
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-03-28 22:12:11 +0700

    Add Attack Animation

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        attack();
+    }
+
+    void attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isHit", true);
+            anim.SetBool("isLeftClick", true);
+        }else if (Input.GetMouseButtonUp(0))
+        {
+            resetAnimation();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anim.SetBool("isHit", true);
+            anim.SetBool("isRightClick", true);
+        }else if (Input.GetMouseButtonUp(1))
+        {
+            resetAnimation();
+        }
+    }
+
+    public void resetAnimation()
+    {
+        anim.SetBool("isHit", false);
+        anim.SetBool("isRightClick", false);
+        anim.SetBool("isLeftClick", false);
+    }
+}
