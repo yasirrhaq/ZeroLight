17b1bf069653759ddfd72353ec109abf8b9b5405
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-04-11 23:44:33 +0700

    Update Enemy.cs

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -2,55 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
+    public string enemyName;
+
     public int enemyHealth;
+    public int damage;
+
     public float enemyMovementSpeed;
+
     public float startDazedTime;
-    private float dazedTime;
+    public float dazedTime;
 
-    public Vector3 moveDirectionPush;
-    public Rigidbody2D EnemyRigidBody;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        EnemyRigidBody = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (dazedTime <= 0)
-        {
-            enemyMovementSpeed = 1.0f;
-        }
-        else
-        {
-            enemyMovementSpeed = 0;
-            dazedTime -= Time.deltaTime;
-        }
-
-        if (enemyHealth <=0)
-        {
-            Destroy(gameObject);
-        }
-        //Ini dummy cmn test gerak
-        transform.position += Vector3.left *enemyMovementSpeed *Time.deltaTime;
-    }
-
-    public void TakeDamage(int damage)//, int force)
-    {
-        dazedTime = startDazedTime;
-        enemyHealth -= damage;
-        Debug.Log("Damage Taken : " + damage + " Enemy Health : "+ enemyHealth);
-        //Knockback(force);
-    }
-
-    //Premature
-    //public void Knockback(int force)
-    //{
-    //    EnemyRigidBody.AddForce(new Vector2(2, 0) * force);
-    //}
+    public abstract void TakeDamage(int damage);
 }
