a7a28426860be2dd88df4fb3c9d2cfa91a1ba87d
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-04-11 10:03:54 +0700

    Add animation

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -7,7 +7,7 @@ public class CharacterController : MonoBehaviour
     public GameObject character;
     public AnimationController animationController;
 
-    //Movement
+    
     public float movementSpeed;
     [HideInInspector]
     public float horizontalMove = 0f;
@@ -15,23 +15,25 @@ public class CharacterController : MonoBehaviour
     public float verticalMove = 0f;
     private Vector3 moveDir;
 
-    //Attack
-    //private float timeBtwAttack;
-    //public float startTimeBtwAttack;
+
     public int damage;
     public float attackRange;
     public Transform attackPosition;
     public LayerMask whatIsEnemies;
 
+    private Rigidbody2D rbd2;
+    private Animator anim;
+
     private void Start()
     {
-        
+        rbd2 = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
-        verticalMove = Input.GetAxisRaw("Vertical") * movementSpeed;
+      
+
         PlayerAttack();
     }
 
@@ -48,16 +50,23 @@ public class CharacterController : MonoBehaviour
 
     void PlayerMovement()
     {
-        moveDir = new Vector2(horizontalMove, verticalMove).normalized;
-        //animationController.Walking();
-        transform.position += moveDir * movementSpeed * Time.deltaTime;
+        rbd2.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movementSpeed;
+
+        anim.SetFloat("MoveX", rbd2.velocity.x);
+        anim.SetFloat("MoveY", rbd2.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+
     }
 
     void PlayerAttack()
     {
 
-        //if (timeBtwAttack <= 0)
-       // {
             if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
             {
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemies);
@@ -66,12 +75,7 @@ public class CharacterController : MonoBehaviour
                 enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);//, 3);
                 }
             }
-       //     timeBtwAttack = startTimeBtwAttack;
-       // }
-       // else
-       // {
-      //      timeBtwAttack -= Time.deltaTime;
-      //  }
+
     }
 
 }
