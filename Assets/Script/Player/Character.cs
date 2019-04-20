68b7a29d13615f3f568e9f460573f3ab0ccdaa8e
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-04-20 12:20:02 +0700

    Create Character.cs

---
+++ Assets/Script/Player/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public string characterName;
+
+    public int playerHealth;
+    public int damage;
+
+    public float movementSpeed;
+
+    public abstract void TakeDamage(int damage);
+    public abstract void Attack(int damage);
+}
